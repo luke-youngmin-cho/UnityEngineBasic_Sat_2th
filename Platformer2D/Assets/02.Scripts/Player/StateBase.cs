5a2e2af3c340d523db9cfbff6068e254dade08c3
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-10-01 18:49:05 +0900

    Platformer2D
    Created
    - Drew StateMachine concept (draw.io)
    - Created StateMachine, IState, StateBase, StateBase Extensions
    - Usage of reflection
    - Ground detector
    - Rigidbody collision

---
+++ Platformer2D/Assets/02.Scripts/Player/StateBase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateBase : IState
+{
+    protected StateMachine.StateType MachineType;
+    protected StateMachine Machine;
+    protected Animator Animator;
+    public StateBase(StateMachine.StateType machineType, StateMachine machine)
+    {
+        MachineType = machineType;
+        Machine = machine;
+        Animator = Machine.GetComponent<Animator>();
+    }
+
+    public IState.Commands Current { get; protected set; }
+
+    public abstract bool IsExecuteOK { get; }
+
+    public abstract void Execute();
+
+    public abstract void FixedUpdate();
+
+    public abstract void ForceStop();
+
+    public abstract void MoveNext();
+
+    public abstract StateMachine.StateType Update();
+}
